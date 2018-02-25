3b5c02aa70ffe5b5d2803f124b2477e00c4ca943
Biana Stepanova <biana.stepanova@mail.ru>
2018-02-25 19:44:32 +0700

    no message

---
+++ tour-agency/InfTourForm.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace tour_agency
+{
+    partial class InfTourForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBoxCountry = new System.Windows.Forms.ComboBox();
+            this.comboBoxCity = new System.Windows.Forms.ComboBox();
+            this.comboBoxHotel = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBoxEx = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBoxCostHotel = new System.Windows.Forms.ListBox();
+            this.listBoxCostEx = new System.Windows.Forms.ListBox();
+            this.listBoxTimeEx = new System.Windows.Forms.ListBox();
+            this.labelTextHotel = new System.Windows.Forms.Label();
+            this.labelTextEx = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(224, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Информация о турах";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(26, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Страна:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(34, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Город:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(34, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Отель:";
+            // 
+            // comboBoxCountry
+            // 
+            this.comboBoxCountry.FormattingEnabled = true;
+            this.comboBoxCountry.Location = new System.Drawing.Point(93, 46);
+            this.comboBoxCountry.Name = "comboBoxCountry";
+            this.comboBoxCountry.Size = new System.Drawing.Size(167, 23);
+            this.comboBoxCountry.TabIndex = 4;
+            // 
+            // comboBoxCity
+            // 
+            this.comboBoxCity.FormattingEnabled = true;
+            this.comboBoxCity.Location = new System.Drawing.Point(93, 86);
+            this.comboBoxCity.Name = "comboBoxCity";
+            this.comboBoxCity.Size = new System.Drawing.Size(167, 23);
+            this.comboBoxCity.TabIndex = 5;
+            // 
+            // comboBoxHotel
+            // 
+            this.comboBoxHotel.FormattingEnabled = true;
+            this.comboBoxHotel.Location = new System.Drawing.Point(93, 120);
+            this.comboBoxHotel.Name = "comboBoxHotel";
+            this.comboBoxHotel.Size = new System.Drawing.Size(167, 23);
+            this.comboBoxHotel.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(290, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Экскурсия:";
+            // 
+            // comboBoxEx
+            // 
+            this.comboBoxEx.FormattingEnabled = true;
+            this.comboBoxEx.Location = new System.Drawing.Point(378, 119);
+            this.comboBoxEx.Name = "comboBoxEx";
+            this.comboBoxEx.Size = new System.Drawing.Size(161, 23);
+            this.comboBoxEx.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Стоимость:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(6, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(81, 19);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Описание:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(286, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(86, 19);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Стоимость:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(318, 213);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(54, 19);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Время:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(291, 249);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 19);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "Описание:";
+            // 
+            // listBoxCostHotel
+            // 
+            this.listBoxCostHotel.FormattingEnabled = true;
+            this.listBoxCostHotel.ItemHeight = 15;
+            this.listBoxCostHotel.Location = new System.Drawing.Point(93, 173);
+            this.listBoxCostHotel.Name = "listBoxCostHotel";
+            this.listBoxCostHotel.Size = new System.Drawing.Size(167, 19);
+            this.listBoxCostHotel.TabIndex = 14;
+            // 
+            // listBoxCostEx
+            // 
+            this.listBoxCostEx.FormattingEnabled = true;
+            this.listBoxCostEx.ItemHeight = 15;
+            this.listBoxCostEx.Location = new System.Drawing.Point(378, 173);
+            this.listBoxCostEx.Name = "listBoxCostEx";
+            this.listBoxCostEx.Size = new System.Drawing.Size(161, 19);
+            this.listBoxCostEx.TabIndex = 16;
+            // 
+            // listBoxTimeEx
+            // 
+            this.listBoxTimeEx.FormattingEnabled = true;
+            this.listBoxTimeEx.ItemHeight = 15;
+            this.listBoxTimeEx.Location = new System.Drawing.Point(378, 213);
+            this.listBoxTimeEx.Name = "listBoxTimeEx";
+            this.listBoxTimeEx.Size = new System.Drawing.Size(161, 19);
+            this.listBoxTimeEx.TabIndex = 17;
+            // 
+            // labelTextHotel
+            // 
+            this.labelTextHotel.AutoSize = true;
+            this.labelTextHotel.Location = new System.Drawing.Point(93, 217);
+            this.labelTextHotel.Name = "labelTextHotel";
+            this.labelTextHotel.Size = new System.Drawing.Size(26, 15);
+            this.labelTextHotel.TabIndex = 18;
+            this.labelTextHotel.Text = "text";
+            // 
+            // labelTextEx
+            // 
+            this.labelTextEx.AutoSize = true;
+            this.labelTextEx.Location = new System.Drawing.Point(391, 252);
+            this.labelTextEx.Name = "labelTextEx";
+            this.labelTextEx.Size = new System.Drawing.Size(26, 15);
+            this.labelTextEx.TabIndex = 19;
+            this.labelTextEx.Text = "text";
+            // 
+            // InfTourForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 360);
+            this.Controls.Add(this.labelTextEx);
+            this.Controls.Add(this.labelTextHotel);
+            this.Controls.Add(this.listBoxTimeEx);
+            this.Controls.Add(this.listBoxCostEx);
+            this.Controls.Add(this.listBoxCostHotel);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBoxEx);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxHotel);
+            this.Controls.Add(this.comboBoxCity);
+            this.Controls.Add(this.comboBoxCountry);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Name = "InfTourForm";
+            this.Text = "InfTourForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBoxCountry;
+        private System.Windows.Forms.ComboBox comboBoxCity;
+        private System.Windows.Forms.ComboBox comboBoxHotel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBoxEx;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBoxCostHotel;
+        private System.Windows.Forms.ListBox listBoxCostEx;
+        private System.Windows.Forms.ListBox listBoxTimeEx;
+        private System.Windows.Forms.Label labelTextHotel;
+        private System.Windows.Forms.Label labelTextEx;
+    }
+}
